fd17861125e9144c2bd5ca5fafb21c5b06ba02d7
r-kitaev <r.kitaev.dev@gmail.com>
2010-09-15 18:30:50 +0400

    -- Fix audit db connection for postgres driver (statement number; query properties)

--- dbal/src/drivers/lmbAuditDbConnection.class.php
+++ dbal/src/drivers/lmbAuditDbConnection.class.php
@@ -2,10 +2,11 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
+
 lmb_require('limb/dbal/src/drivers/lmbDbConnection.interface.php');
 lmb_require('limb/core/src/lmbBacktrace.class.php');
 lmb_require('limb/core/src/lmbDecorator.class.php');
@@ -20,51 +21,59 @@ lmbDecorator :: generate('lmbDbConnection', 'lmbDbConnectionDecorator');
  */
 class lmbAuditDbConnection extends lmbDbConnectionDecorator
 {
-  protected $stats = array();
-  
+  protected $_stats = array();
+  protected $_statement_number = 0;
+
   function execute($sql)
   {
-    $info = array('query' => $sql);
+    $info = array();
+    $info['query'] = $sql;
     $info['trace'] = $this->getTrace();
+
     $start_time = microtime(true);
     $res = parent :: execute($sql);
     $info['time'] = round(microtime(true) - $start_time, 6);
-    $this->stats[] = $info;
+
+    $this->_stats[] = $info;
     return $res;
   }
-  
+
   function executeStatement($stmt)
   {
-    $info = array('query' => $stmt->getSQL());
+    $info = array();
     $info['trace'] = $this->getTrace();
+
     $start_time = microtime(true);
     $res = parent :: executeStatement($stmt);
     $info['time'] = round(microtime(true) - $start_time, 6);
-    $this->stats[] = $info;
+
+    $info['query'] = $this->_replaceProperties($stmt->getSQL(), $stmt->getParameters());
+
+    $this->_stats[] = $info;
     return $res;
   }
-  
+
   function newStatement($sql)
   {
     $statement = parent :: newStatement($sql);
     $statement->setConnection($this);
     return $statement;
   }
-  
+
   function countQueries()
   {
-    return sizeof($this->stats);
+    return sizeof($this->_stats);
   }
-  
+
   function resetStats()
   {
-    $this->stats = array();
+    $this->_stats = array();
   }
-  
+
   function getQueries($reg_exp = '')
   {
     $res = array();
-    foreach($this->stats as $info)
+    foreach($this->_stats as $info)
     {
       $query = $info['query'];
       if(!$reg_exp || preg_match('/' . $reg_exp . '/i', $query))
@@ -73,22 +82,32 @@ class lmbAuditDbConnection extends lmbDbConnectionDecorator
 
     return $res;
   }
-  
-  function getTrace() 
+
+  function getTrace()
   {
-  	$trace_length = 8;
-  	$offset = 4; // getting rid of useless trace elements
-  	
-  	$trace = new lmbBacktrace($trace_length, $offset);
-  	return $trace->toString();
+    $trace_length = 8;
+    $offset = 4; // getting rid of useless trace elements
+
+    $trace = new lmbBacktrace($trace_length, $offset);
+    return $trace->toString();
   }
-  
+
   function getStats()
   {
-    return $this->stats; 
+    return $this->_stats;
   }
-  
-  
+
+  function getStatementNumber()
+  {
+    return ++$this->_statement_number;
+  }
+
+  protected function _replaceProperties($sql, $parameters)
+  {
+    $keys = array();
+    foreach($parameters as $key => $value)
+      $keys[] = ":{$key}:";
+
+    return str_replace($keys, $parameters, $sql);
+  }
 }
-
-
